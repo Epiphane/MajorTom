2d95a2201ef725a7b160939e4b74ff69354e8d36
Thomas Steinke <exyphnos@gmail.com>
2017-01-21 11:49:50 -0800

    Using UnityEvent for button and lever controls

---
+++ Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonControl : MonoBehaviour {
+	public UnityEvent shipFunction;
+
+	private void Start()
+	{
+		if (shipFunction == null) {
+			shipFunction = new UnityEvent ();
+		}
+	}
+
+	private void handlePush()
+	{
+		shipFunction.Invoke ();
+	}
+}
--- Assets/Scripts/LeverControl.cs
+++ Assets/Scripts/LeverControl.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LeverControl : MonoBehaviour {
-
-    public ShipManagerScript ship;
-    public int leverNum;    
+	
+	public UnityEvent shipFunction;
 
     private void Start()
-    {
+	{
+		if (shipFunction == null) {
+			shipFunction = new UnityEvent ();
+		}
+
         GetComponent<VRTK.VRTK_Control>().defaultEvents.OnValueChanged.AddListener(HandleChange);
         HandleChange(GetComponent<VRTK.VRTK_Control>().GetValue(), GetComponent<VRTK.VRTK_Control>().GetNormalizedValue());
     }
 
-    private void HandleChange(float value, float normalizedValue)
+	private void HandleChange(float value, float normalizedValue)
     {
-        ship.PullLever(leverNum);
+		for(int i = 0 ; i < shipFunction.GetPersistentEventCount();i++ ){    
+			((MonoBehaviour)shipFunction.GetPersistentTarget(i))
+				.SendMessage(shipFunction.GetPersistentMethodName(i), normalizedValue);
+		}
     }
 }
