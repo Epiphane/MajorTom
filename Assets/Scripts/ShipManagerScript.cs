23840f190b414f04ef09a905477945713534f186
Thomas Steinke <exyphnos@gmail.com>
2017-01-22 15:08:48 -0800

    Doogo Deux

--- Assets/Scripts/ShipManagerScript.cs
+++ Assets/Scripts/ShipManagerScript.cs
@@ -196,10 +196,10 @@ public class ShipManagerScript : MonoBehaviour {
 	}
 
 	public void IgniteEngine () {
-        Debug.Log("ENGINE ON");
+		if (!engineOn)
+			engineOnSound.Play ();
+		
         engineOn = true;
-
-		engineOnSound.Play ();
 	}
 
     public void DisengageBrakes() {
