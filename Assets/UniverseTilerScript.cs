cd350981c64407024c01ab9ba88eb64e45cdd0b7
Elliot Fiske <elliotfiske@gmail.com>
2017-01-22 13:55:12 -0800

    YAY

---
+++ Assets/UniverseTilerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniverseTilerScript : MonoBehaviour {
+
+    public List<GameObject> tilarinos;
+
+    public float offset = 0;
+
+	// Use this for initialization
+	void Start () {
+        tilarinos.Sort
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangePos(Vector3 delta) {
+        offset += delta.z;
+        transform.Translate(delta);
+
+        if (offset > 675) {
+            offset -= 675;
+        }
+    }
+}
